e26dd70db9dccbc621929416ce3c7c163b2c6bae
aleyacorretjer <ac63489p@pace.edu>
2021-12-07 22:31:37 -0500

    Project 2

---
+++ Project2/Project2/WhoWroteIt/app/src/test/java/FetchBook.java
@@ -0,0 +1,16 @@
+public class FetchBook extends AsyncTask<String, Void, String>{
+    public FetchBook(TextView mTitleText, TextView mAuthorText) {
+        this.mTitleText = new WeakReference<>(titleText);
+        this.mAuthorText = new WeakReference<>(authorText);
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+}
